bba501f67eb7ab8b633f170c9b7e84fa72c64d55
Thzzao <thiago.santos1160@gmail.com>
2023-08-30 16:49:19 -0300

    feat; Criação da função que remove números da lista

--- app.js
+++ app.js
@@ -2,31 +2,21 @@
 import * as rl from 'readline-sync';
 
 //Instrução
-console.log("Digite pelo menos 3 propriedades do CSS.\nPara ordenar digite 'sair'\n")
+console.log("Digite pelo menos 2 propriedades do CSS.\nPara ordenar digite 'sair'\n")
 
 //Criação do array e da variável
 const props = []
 let input
-
-
 
 //Entrada do usuário e tratamento de erros
 do {
     input = (rl.question('Digite a propriedade: ')).toLowerCase()
     //Guarda a entrada do usuário na lista
     props.push(input)
-
-    /*if (input   == " " | null) {
-        console.log("Espaços vazios não são permitidos")
-    }
-    else if (input == num) {
-        console.log("Numeros não são permitidos")
-    }*/
 } while (input != 'sair')
 
-
-
-
+// Chamada da função que remove os números do array 
+removerNumeros()
 // Chamada da função que remove os espaços vazios
 removerNulo()
 // Remove o último item da lista (a palavra 'sair')
@@ -40,11 +30,26 @@ props.forEach(function (element) {
 })
 
 
+
 //Criação da função de excluir os espaços vazios 
 function removerNulo() {
     var index = props.indexOf('')
     while (index >= 0) {
         props.splice(index, 1);
         index = props.indexOf('')
+    }
+}
+
+// Criação da função que remove números
+function removerNumeros() {
+    let numeros = props.some((number) => {
+        return number >= 0; //tem que ser todos os números, não somente o zero ou os maiores que zero <<<<<<<<<<<
+    })
+    while (numeros == true) {
+        const found = props.findIndex((number) => number >= 0)
+        props.splice(found, 1)
+        if (found == -1) {
+            break
+        }
     }
 }
