9226af9598b60a53dae88a6f59647628f1f8f839
Thzzao <thiago.santos1160@gmail.com>
2023-08-30 00:51:26 -0300

    refactor: Criada a Iteração dos resultados em forma de lista e comentários no código

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
+// Importando o módulo readline-sync
 import * as rl from 'readline-sync';
 
+//Instrução
 console.log("Digite pelo menos 3 propriedades do CSS. Para ordenar digite 'SAIR'")
 
+//Criação do array e da variável
 const props = []
-
 let input
 
+//Entrada do usuário e tratamento de erros
 do {
     input = (rl.question('Digite a propriedade: ')).toUpperCase();
+    //Guarda a entrada do usuário na lista
     props.push(input);
+    /*
+    if (input == " ") {
 
+    }
+    else if (input == number) {
+
+    }*/
 } while (input != 'SAIR');
 
+// Remove o último item da lista (a palavra 'sair')
 props.pop()
+// Ordena as palavras em ordem alfabética crescente 
 props.sort()
-console.log(props)
+// Imprime os elementos em forma de lista
+props.forEach(function (element) {
+    console.log(element)
+})
